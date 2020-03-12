b6e8a2ae727326ad8c0547f2c360c3ee0162df4e
hernad <hernad@bring.out.ba>
2020-03-12 13:44:40 +0100

    build on fedora 31

---
+++ scripts/fedora_build.sh
@@ -0,0 +1,14 @@
+sudo dnf -y install meson
+
+PACKAGES=cppunit-devel
+PACKAGES+=liblangtag-devel libicu-devel nss-devel boost-devel
+PACKAGES+=harfbuzz-devel glm-devel cairo-devel libeot-devel libjpeg-devel
+PACKAGES+=lcms2-devel libepoxy-devel gtk3-devel liburcus-devel clucene-core-deve libxslt-devel
+PACKAGES+=libqrcodegen-devel libqrcodegencpp-devel librevenge-devel libodfgen-devel libepubgen-devel
+PACKAGES+=libstaroffice-devel libabw-devel libmwaw-devel libetonyek-devel libwps-devel libwpd-devel libwpg-devel
+PACKAGES+=libe-book-devel raptor2-devel rasqal-devel liblrdf-devel redland-devel libcurl-devel
+PACKAGES+=hunspell-devel hyphen-devel mythes-devel libexttextcat-deve mdds-devel
+
+
+
+PACKAGES+=sane-backends-devel
