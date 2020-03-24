782e860de1ce5d29ba6d843b1193b29664ed96e3
Ernad Husremovic <hernad@bring.out.ba>
2020-03-24 09:55:31 +0100

    build LO_Ziher release

---
+++ scripts/build_lo_release.sh
@@ -0,0 +1,12 @@
+make clean
+
+./autogen.sh --with-lang="bs" \
+   --with-locales="bs" \
+   --with-vendor="hernad" \
+   --with-theme="colibre_svg" \
+    --with-visual-studio=2019 \
+    --without-doxygen \
+    --without-java \
+    --with-product-name="LO_Ziher"\
+    --with-package-version='7.0.0.10'\
+    -–enable-release-build
