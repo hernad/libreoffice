13502955133f4305d31ce4158ed3e11c52fc3df6
Ernad Husremovic <hernad@bring.out.ba>
2020-04-20 21:45:35 +0200

    svtzh_svxcorezh must be first patch

--- scripts/patch_zh.sh
+++ scripts/patch_zh.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+
+patch -p1 < patch/00_svtzh_svxcorezh.diff
 patch -p1 < patch/01_DLL_FNAME_POSTFIX.diff
 patch -p1 < patch/02_sfxzh.diff
 patch -p1 < patch/03_mk_lo2zh.diff
 patch -p1 < patch/launcher.diff
-patch -p1 < patch/svtzh_svxcorezh.diff
 
 #patch -p1 < patch/04_mk_solenv.diff
 #patch -p1 < patch/05_configure_ac.diff
