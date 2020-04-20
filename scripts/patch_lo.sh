957f5f3d7f4235f8aafabc651e56ff27b558cd59
Ernad Husremovic <hernad@bring.out.ba>
2020-04-20 21:24:28 +0200

    patch_lo, patch_zh.sh

---
+++ scripts/patch_lo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#patch -p1 < patch/01_DLL_FNAME_POSTFIX.diff
+#patch -p1 < patch/02_sfxzh.diff
+#patch -p1 < patch/03_mk_lo2zh.diff
+#patch -p1 < patch/launcher.diff
+
+patch -p1 < patch/04_mk_solenv.diff
+patch -p1 < patch/05_configure_ac.diff
+patch -p1 < patch/06_mk_modules.diff
+patch -p1 < patch/ado_cut.patch
+patch -p1 < patch/help.diff
+patch -p1 < patch/init_bs.diff
