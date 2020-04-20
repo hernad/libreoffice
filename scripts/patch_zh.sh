0180269730f3a1527080a47bb1c85b8d70500e4c
Ernad Husremovic <hernad@bring.out.ba>
2020-04-20 21:37:08 +0200

    svtlo, svxcorelo - svtzh, svxcorezh

--- scripts/patch_zh.sh
+++ scripts/patch_zh.sh
@@ -4,6 +4,7 @@ patch -p1 < patch/01_DLL_FNAME_POSTFIX.diff
 patch -p1 < patch/02_sfxzh.diff
 patch -p1 < patch/03_mk_lo2zh.diff
 patch -p1 < patch/launcher.diff
+patch -p1 < patch/svtzh_svxcorezh.diff
 
 #patch -p1 < patch/04_mk_solenv.diff
 #patch -p1 < patch/05_configure_ac.diff
