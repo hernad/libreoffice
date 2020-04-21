e15d68dcd6542ea9bb7fd1b9a92cb1acea008a08
Ernad Husremovic <hernad@bring.out.ba>
2020-04-21 16:05:08 +0200

    conan files

---
+++ conan/install_x64.cmd
@@ -0,0 +1,20 @@
+rm -rf c:/dev/libreoffice/conan/deploy_x64
+
+REM C:\dev\set_cmake_path.cmd
+
+conan install .. --install-folder c:/dev/libreoffice/conan/deploy_x64 ^
+     --settings arch=x86_64 ^
+     --settings compiler.version="16" ^
+     --build zlib ^
+     --build libpng ^
+     --build boost ^
+     --build libpq ^
+     --build icu ^
+     --build libxml2 ^
+     --build libxslt ^
+     --build openssl ^
+     --build libcurl ^
+     --build libjpeg-turbo
+
+
+REM     --settings arch=x86_64 --settings compiler.runtime="MT" --settings compiler.version="16"
