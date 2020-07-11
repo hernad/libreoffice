e214205454665ba48d1d20950809c90258a66563
Ernad Husremović <ernad.husremovic@SA.OUT.BA>
2020-07-11 09:56:42 +0100

    set win32 path

--- scripts/set_win32_path.sh
+++ scripts/set_win32_path.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-echo set make win32
+
+mkdir -p scripts/win32
+cd scripts/win32
+if [ ! -f make.exe ] ; then
+  curl -L https://dev-www.libreoffice.org/bin/cygwin/make-4.2.1-msvc.exe -o make.exe
+  chmod +x make.exe
+fi
+cd ../..
+
+echo set make win32 PATH
 
 export PATH=`pwd`/scripts/win32:$PATH
 
