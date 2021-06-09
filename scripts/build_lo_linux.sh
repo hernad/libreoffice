c57639b339580206307c3d09c61817f488d92483
Ernad Husremovic <hernad@bring.out.ba>
2021-06-09 12:58:26 +0200

    build linux

--- scripts/build_lo_linux.sh
+++ scripts/build_lo_linux.sh
@@ -3,46 +3,53 @@
 #RUN
 #ENVARS_ONLY=0 MAKE_ONLY=0 PARALLELISM=12 scripts/build_lo_linux.sh
 
+echo "========== fedora ====================="
+echo "sudo dnf install libICE-devel libSM-devel libXt-devel gstreamer1-devel ant"
+
 
 THEME=colibre
 #THEME=sukapura_svg
 
 LO_PRODUCT_NAME=ZiherO
-LO_PRODUCT_VERSION=7.0.0.530
+LO_PRODUCT_VERSION=7.1.5.100
 
 #LO_DEBUG=" --enable-dbgutil"
 
-ENABLE_64_BIT=--enable-64-bit
-export PYTHON_VERSION_MAJOR=3
-export PYTHON_VERSION_MINOR=7
-export PYTHON_VERSION="3.7.7"
-export CONAN_DEPLOY_DIR=$HOME/libreoffice/conan/deploy_x64
-export PYTHON_ROOT=$HOME/python${PYTHON_VERSION_MAJOR}${PYTHON_VERSION_MINOR}
-export PYTHON=${PYTHON_ROOT}/bin/python${PYTHON_VERSION_MAJOR}
-export PYTHON_CFLAGS="-I${PYTHON_ROOT}/include/python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}m" # python3.7m
-export PYTHON_LIBS="-L${PYTHON_ROOT}/lib -lpython${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}m "  # libpython3.7m.so
+#ENABLE_64_BIT=--enable-64-bit
+
+#export PYTHON_VERSION_MAJOR=3
+#export PYTHON_VERSION_MINOR=7
+#export PYTHON_VERSION="3.7.7"
+#export CONAN_DEPLOY_DIR=$HOME/libreoffice/conan/deploy_x64
+#export PYTHON_ROOT=$HOME/python${PYTHON_VERSION_MAJOR}${PYTHON_VERSION_MINOR}
+#export PYTHON=${PYTHON_ROOT}/bin/python${PYTHON_VERSION_MAJOR}
+#export PYTHON_CFLAGS="-I${PYTHON_ROOT}/include/python${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}m" # python3.7m
+#export PYTHON_LIBS="-L${PYTHON_ROOT}/lib -lpython${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}m "  # libpython3.7m.so
   
 #BOOST_NODEFAULT_SUFIX="vc142-mt-x64-1_72.lib"
 #JAVA_DIR=/cygdrive/c/openjdk-panama-foreign/x64/jdk
 
-JAVA_HOME="$HOME/jdk dk-14.0.1+7"
+JAVA_HOME="/usr/lib/jvm/java-11-openjdk"
 #JAVA_HOME="$HOME/openjdk-panama-foreign/jdk"
-ANT_HOME="$HOME/apache-ant-1.9.14"
+#ANT_HOME="$HOME/apache-ant-1.9.14"
 JUNIT4="$HOME/junit-4.13.jar"
 HAMCREST="$HOME/hamcrest-2.2.jar"
 
 # /usr/sbin/gencmn.exe
-export PATH=$JAVA_HOME/bin:$ANT_HOME/bin:$PATH
-export PATH=$HOME/$PYTHON_ROOT/bin:$PATH
-export LD_LIBRARY_PATH=$PYTHON_ROOT/lib:$LD_LIBRARY_PATH 
+#export PATH=$JAVA_HOME/bin:$ANT_HOME/bin:$PATH
+export PATH=$JAVA_HOME/bin:$PATH
+
+#export PATH=$HOME/$PYTHON_ROOT/bin:$PATH
+#export LD_LIBRARY_PATH=$PYTHON_ROOT/lib:$LD_LIBRARY_PATH 
 
 export JAVA_HOME
-echo "JAVA_HOME=$JAVA_HOME ANT_HOME=$ANT_HOME"
+#echo "JAVA_HOME=$JAVA_HOME ANT_HOME=$ANT_HOME"
 java -version
-$ANT_HOME/bin/ant -version
+#$ANT_HOME/bin/ant -version
 
 #JAVA_FEATURE="--without-java"
-JAVA_FEATURE=" --with-jdk-home=$JAVA_HOME --with-ant-home=$ANT_HOME --with-junit=$JUNIT4 --with-hamcrest=$HAMCREST"
+#JAVA_FEATURE=" --with-jdk-home=$JAVA_HOME --with-ant-home=$ANT_HOME --with-junit=$JUNIT4 --with-hamcrest=$HAMCREST"
+JAVA_FEATURE=" --with-jdk-home=$JAVA_HOME --without-junit"
 
 
 # ---------------------------------------------------------
@@ -88,75 +95,77 @@ WITH_SYSTEM=
 echo PYTHON_CFLAGS=$PYTHON_CFLAGS, PYTHON_LIBS=$PYTHON_LIBS
 WITH_SYSTEM+=" --enable-python=system"
 
-WITH_SYSTEM+=" --with-system-zlib=yes"
-export ZLIB_CFLAGS="-I$CONAN_DEPLOY_DIR/zlib/include"
-export ZLIB_LIBS="-L$CONAN_DEPLOY_DIR/zlib/lib -lz"
+#WITH_SYSTEM+=" --with-system-zlib=yes"
+#export ZLIB_CFLAGS="-I$CONAN_DEPLOY_DIR/zlib/include"
+#export ZLIB_LIBS="-L$CONAN_DEPLOY_DIR/zlib/lib -lz"
  
-WITH_SYSTEM+=" --with-system-libpng=yes"
-export LIBPNG_CFLAGS="-I$CONAN_DEPLOY_DIR/libpng/include"
-export LIBPNG_LIBS="-L$CONAN_DEPLOY_DIR/libpng/lib -lpng16 $ZLIB_LIBS"
+#WITH_SYSTEM+=" --with-system-libpng=yes"
+#export LIBPNG_CFLAGS="-I$CONAN_DEPLOY_DIR/libpng/include"
+#export LIBPNG_LIBS="-L$CONAN_DEPLOY_DIR/libpng/lib -lpng16 $ZLIB_LIBS"
 
-WITH_SYSTEM+=" --with-system-jpeg=yes"
-export LIBJPEG_CFLAGS="-I$CONAN_DEPLOY_DIR/libjpeg-turbo/include"
-export LIBJPEG_LIBS="-L$CONAN_DEPLOY_DIR/libjpeg-turbo/lib -lturbojpeg $ZLIB_LIBS"
+#WITH_SYSTEM+=" --with-system-jpeg=yes"
+#export LIBJPEG_CFLAGS="-I$CONAN_DEPLOY_DIR/libjpeg-turbo/include"
+#export LIBJPEG_LIBS="-L$CONAN_DEPLOY_DIR/libjpeg-turbo/lib -lturbojpeg $ZLIB_LIBS"
 
-WITH_SYSTEM+=" --with-system-boost"
-export BOOST_CPPFLAGS="-I$CONAN_DEPLOY_DIR/boost/include -I$CONAN_DEPLOY_DIR/bzip2/include"
-export BOOST_CXXFLAGS=$BOOST_CPPFLAGS
+#WITH_SYSTEM+=" --with-system-boost"
+#export BOOST_CPPFLAGS="-I$CONAN_DEPLOY_DIR/boost/include -I$CONAN_DEPLOY_DIR/bzip2/include"
+#export BOOST_CXXFLAGS=$BOOST_CPPFLAGS
   
 
-BZIP2_LIB="-L$CONAN_DEPLOY_DIR/bzip2/lib -lbz2"
-export BOOST_ROOT="$CONAN_DEPLOY_DIR/boost"
-export BOOST_LOCALE_LIB="$BZIP2_LIB -L$CONAN_DEPLOY_DIR/boost/lib -lboost_locale $ZLIB_LIBS"
-export BOOST_DATE_TIME_LIB="$BZIP2_LIB -L$CONAN_DEPLOY_DIR/boost/lib -lboost_date_time $ZLIB_LIBS"
-export BOOST_FILESYSTEM_LIB="$BZIP2_LIB -L$CONAN_DEPLOY_DIR/boost/lib -lboost_filesystem $ZLIB_LIBS"
-export BOOST_IOSTREAMS_LIB="$BZIP2_LIB -L$CONAN_DEPLOY_DIR/boost/lib -lboost_iostreams $ZLIB_LIBS"
-export BOOST_SYSTEM_LIB="$BZIP2_LIB -L$CONAN_DEPLOY_DIR/boost/lib -lboost_system $ZLIB_LIBS"
-export BOOST_LDFLAGS="$BOOST_LOCALE_LIB $BOOST_DATE_TIME_LIB $BOOST_FILESYSTEM_LIB $BOOST_IOSTREAMS_LIB $BOOST_SYSTEM_LIB"
+#BZIP2_LIB="-L$CONAN_DEPLOY_DIR/bzip2/lib -lbz2"
+#export BOOST_ROOT="$CONAN_DEPLOY_DIR/boost"
+#export BOOST_LOCALE_LIB="$BZIP2_LIB -L$CONAN_DEPLOY_DIR/boost/lib -lboost_locale $ZLIB_LIBS"
+#export BOOST_DATE_TIME_LIB="$BZIP2_LIB -L$CONAN_DEPLOY_DIR/boost/lib -lboost_date_time $ZLIB_LIBS"
+#export BOOST_FILESYSTEM_LIB="$BZIP2_LIB -L$CONAN_DEPLOY_DIR/boost/lib -lboost_filesystem $ZLIB_LIBS"
+#export BOOST_IOSTREAMS_LIB="$BZIP2_LIB -L$CONAN_DEPLOY_DIR/boost/lib -lboost_iostreams $ZLIB_LIBS"
+#export BOOST_SYSTEM_LIB="$BZIP2_LIB -L$CONAN_DEPLOY_DIR/boost/lib -lboost_system $ZLIB_LIBS"
+#export BOOST_LDFLAGS="$BOOST_LOCALE_LIB $BOOST_DATE_TIME_LIB $BOOST_FILESYSTEM_LIB $BOOST_IOSTREAMS_LIB $BOOST_SYSTEM_LIB"
 
-WITH_SYSTEM+=" --with-system-openssl=yes"
-export OPENSSL_CFLAGS="-I$CONAN_DEPLOY_DIR/openssl/include"
-export OPENSSL_LIBS="-L$CONAN_DEPLOY_DIR/openssl/lib -lcrypto -lssl"
+#WITH_SYSTEM+=" --with-system-openssl=yes"
+#export OPENSSL_CFLAGS="-I$CONAN_DEPLOY_DIR/openssl/include"
+#export OPENSSL_LIBS="-L$CONAN_DEPLOY_DIR/openssl/lib -lcrypto -lssl"
 
-WITH_SYSTEM+=" --with-system-curl=yes"
-export CURL_CFLAGS="-I$CONAN_DEPLOY_DIR/libcurl/include" 
-export CURL_LIBS="-L$CONAN_DEPLOY_DIR/libcurl/lib -lcurl $OPENSSL_LIBS"
+#WITH_SYSTEM+=" --with-system-curl=yes"
+#export CURL_CFLAGS="-I$CONAN_DEPLOY_DIR/libcurl/include" 
+#export CURL_LIBS="-L$CONAN_DEPLOY_DIR/libcurl/lib -lcurl $OPENSSL_LIBS"
  
-WITH_SYSTEM+=" --with-system-postgresql=yes"
-export POSTGRESQL_CFLAGS="-I$CONAN_DEPLOY_DIR/libpq/include"
-export POSTGRESQL_LIBS="-L$CONAN_DEPLOY_DIR/libpq/lib -lpq $OPENSSL_LIBS"
+#WITH_SYSTEM+=" --with-system-postgresql=yes"
+#export POSTGRESQL_CFLAGS="-I$CONAN_DEPLOY_DIR/libpq/include"
+#export POSTGRESQL_LIBS="-L$CONAN_DEPLOY_DIR/libpq/lib -lpq $OPENSSL_LIBS"
 
-WITH_SYSTEM+=" --with-system-libxml=yes"
-export LIBXML_CFLAGS="-I$CONAN_DEPLOY_DIR/libxml2/include/libxml2 -I$CONAN_DEPLOY_DIR/libiconv/include"
-export LIBXML_LIBS="-L$CONAN_DEPLOY_DIR/libxml2/lib -lxml2" 
-export LIBXML_LIBS+=" $ZLIB_LIBS -L$CONAN_DEPLOY_DIR/libiconv/lib -lcharset -liconv -lm"
-
-export LIBXML_LIBTOOL_LIBS="-L$CONAN_DEPLOY_DIR/libxml2/lib -L$CONAN_DEPLOY_DIR/libiconv/lib -L$CONAN_DEPLOY_DIR/zlib/lib -lxml2 -liconv -lcharset -lz -lm"
-export LIBEXSLT_CFLAGS="-I$CONAN_DEPLOY_DIR/libxslt/include"
-export LIBEXSLT_LIBS="-L$CONAN_DEPLOY_DIR/libxslt/lib -lexslt"
-export LIBXSLT_CFLAGS="-I$CONAN_DEPLOY_DIR/libxslt/include"
-export LIBXSLT_LIBS="-L$CONAN_DEPLOY_DIR/libxslt/lib -lxslt"
-
-WITH_SYSTEM+=" --with-system-xmlsec=yes"
-export XMLSEC_CFLAGS="-I$CONAN_DEPLOY_DIR/xmlsec/include/xmlsec1"
-export XMLSEC_LIBS="-L$CONAN_DEPLOY_DIR/xmlsec/lib -lxmlsec1 -lxmlsec1-openssl"
-
-WITH_SYSTEM+=" --with-system-expat=yes"
-export EXPAT_CFLAGS="-I$CONAN_DEPLOY_DIR/expat/include"
-export EXPAT_LIBS="-L$CONAN_DEPLOY_DIR/expat/lib -lexpat"
-
-export NSS_CFLAGS="-I$CONAN_DEPLOY_DIR/nss/include/nss -I$CONAN_DEPLOY_DIR/nss/include/nspr"
-export NSS_LIBS="-L$CONAN_DEPLOY_DIR/nss/lib -lnspr4 -lnss3 -lssl3 -lsmime3 -lplc4"
-
-export CFLAGS="$NSS_CFLAGS"
-export LDFLAGS="$NSS_LIBS"
-export CPPFLAGS="$CFLAGS"
-export CXXFLAGS="$CFLAGS"
+#WITH_SYSTEM+=" --with-system-libxml=yes"
+#export LIBXML_CFLAGS="-I$CONAN_DEPLOY_DIR/libxml2/include/libxml2 -I$CONAN_DEPLOY_DIR/libiconv/include"
+#export LIBXML_LIBS="-L$CONAN_DEPLOY_DIR/libxml2/lib -lxml2" 
+#export LIBXML_LIBS+=" $ZLIB_LIBS -L$CONAN_DEPLOY_DIR/libiconv/lib -lcharset -liconv -lm"
+#
+#export LIBXML_LIBTOOL_LIBS="-L$CONAN_DEPLOY_DIR/libxml2/lib -L$CONAN_DEPLOY_DIR/libiconv/lib -L$CONAN_DEPLOY_DIR/zlib/lib -lxml2 -liconv -lcharset -lz -lm"
+#export LIBEXSLT_CFLAGS="-I$CONAN_DEPLOY_DIR/libxslt/include"
+#export LIBEXSLT_LIBS="-L$CONAN_DEPLOY_DIR/libxslt/lib -lexslt"
+#export LIBXSLT_CFLAGS="-I$CONAN_DEPLOY_DIR/libxslt/include"
+#export LIBXSLT_LIBS="-L$CONAN_DEPLOY_DIR/libxslt/lib -lxslt"
+#
+#WITH_SYSTEM+=" --with-system-xmlsec=yes"
+#export XMLSEC_CFLAGS="-I$CONAN_DEPLOY_DIR/xmlsec/include/xmlsec1"
+#export XMLSEC_LIBS="-L$CONAN_DEPLOY_DIR/xmlsec/lib -lxmlsec1 -lxmlsec1-openssl"
+#
+#WITH_SYSTEM+=" --with-system-expat=yes"
+#export EXPAT_CFLAGS="-I$CONAN_DEPLOY_DIR/expat/include"
+#export EXPAT_LIBS="-L$CONAN_DEPLOY_DIR/expat/lib -lexpat"
+#
+#export NSS_CFLAGS="-I$CONAN_DEPLOY_DIR/nss/include/nss -I$CONAN_DEPLOY_DIR/nss/include/nspr"
+#export NSS_LIBS="-L$CONAN_DEPLOY_DIR/nss/lib -lnspr4 -lnss3 -lssl3 -lsmime3 -lplc4"
+#
+#export CFLAGS="$NSS_CFLAGS"
+#export LDFLAGS="$NSS_LIBS"
+#export CPPFLAGS="$CFLAGS"
+#export CXXFLAGS="$CFLAGS"
 
 if [ "$MAKE_ONLY" == "0" ]; then
 make clean
 rm -f config_host.mk
 rm -f config_host/*.h
+
+DISABLE="--disable-breakpad --disable-ldap  --disable-gstreamer-1-0"
 
 ./autogen.sh --with-lang="bs" \
    $ENABLE_64_BIT --with-locales="bs" \
@@ -181,8 +190,8 @@ rm -f config_host/*.h
     --disable-sdremote \
     --disable-sdremote-bluetooth \
     $EXTENSIONS $PDF_IMPORT $WEBDAV $WITH_SYSTEM $LO_DEBUG $SKIA_FEATURE $JAVA_FEATURE \
-    --enable-breakpad \
-    --disable-ldap 
+    $DISABLE
+
 
 fi
 
