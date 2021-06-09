becf1c627166d7d829da188b6fdfebdaa2d9ff4c
Ernad Husremovic <hernad@bring.out.ba>
2021-06-09 17:21:33 +0200

    patch/

--- scripts/patch_zh.sh
+++ scripts/patch_zh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # echo ako nije patch_lo.sh primjenjen treba vratiti helplinker and co nazad
-patch -p1 < unpatch_wfpt_helplinker_ziher.diff
+patch -p1 < patch/unpatch_wfpt_helplinker_ziher.diff
 
 patch -p1 < patch/00_svtzh_svxcorezh.diff
 patch -p1 < patch/00b_svxcorezh.diff
@@ -12,6 +12,6 @@ patch -p1 < patch/03_mk_lo2zh.diff
 patch -p1 < patch/launcher.diff
 patch -p1 < patch/zoffice_ini.diff
 
-patch -p1 < svlibrary_DLL_FNAME_POSTFIX.diff
+patch -p1 < patch/svlibrary_DLL_FNAME_POSTFIX.diff
 
 
